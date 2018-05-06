6330f92730c3c598c84ff722a07afc8c00a9ed36
robin7654 <robin.pruefer@tu-dortmund.de>
2018-05-06 21:01:21 +0200

    montec

--- src/MonteCarloSimulation.java
+++ src/MonteCarloSimulation.java
@@ -1,10 +1,12 @@
+import java.util.stream.IntStream;
+
 
 
 public class MonteCarloSimulation {
 
 	public static GameContoller gc = new GameContoller();
 	
-	public int winProbabilityOnRiver(int cards[], int playerNumber, int opponent){
+	public static int winProbabilityOnRiver(int cards[], int playerNumber, int opponent){
 		int[] winners = gc.evaluate();
 		if (winners[playerNumber] > winners[opponent]){
 			return 1;
@@ -14,7 +16,7 @@ public class MonteCarloSimulation {
 		}
 		return 0;
 	}
-	public int winProbabilityOnRiver(int cards[], int playerNumber, int opponent1, int opponent2){
+	public static int winProbabilityOnRiver(int cards[], int playerNumber, int opponent1, int opponent2){
 		int[] winners = gc.evaluate();
 		//verlierer
 		if (winners[playerNumber] < winners[opponent1] | winners[playerNumber] < winners[opponent2]){
@@ -34,10 +36,41 @@ public class MonteCarloSimulation {
 		}
 		return -1; 
 	}
-	public static int winProbabilityOnTurn(){
-		double winProb = 0;
-		return (int) winProb;
+	public static int winProbabilityOnTurn(int cards[], int playerNumber, int opponent1, int opponent2){
+		double wins = 0; 
+		for (int i = 0; i < 52; i++){
+			int a = i;
+			if (IntStream.of(cards).anyMatch(x -> x == a) == false){
+				cards[4]= i;
+				wins = wins + 1/winProbabilityOnRiver(cards, playerNumber, opponent1, opponent2);
+			}
+		}
+		return (int)  (100*wins/42) ;
 	}
+	public static int winProbabilityOnTurn(int cards[], int playerNumber, int opponent){
+		double wins = 0; 
+		for (int i = 0; i < 52; i++){
+			int a = i;
+			if (IntStream.of(cards).anyMatch(x -> x == a) == false){
+				cards[4]= i;
+				wins = wins + 1/winProbabilityOnRiver(cards, playerNumber, opponent);
+			}
+		}
+		return (int)  (100*wins/42) ;
+	}
+//	public static int winProbabilityOnFlop(int cards[], int playerNumber, int opponent1, int opponent2){
+//		double wins = 0; 
+//		for (int i = 0; i < 52; i++){
+//			int a = i;
+//			if (IntStream.of(cards).anyMatch(x -> x == a) == false){
+//				cards[3]= i;
+//				wins = wins + 1/winProbabilityOnTurn(cards, playerNumber, opponent1, opponent2);
+//			}
+//		}
+//		return (int)  (100*wins/42) ;
+//	}
+	
+	
 	
 	
 	
